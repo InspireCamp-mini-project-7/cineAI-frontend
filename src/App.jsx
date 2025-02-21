5d1b2f8cf61185b783d9362b5f9dbd183f294d1d
zzzzzisu <kimjisu4939@gmail.com>
2025-02-21 13:04:12 +0900

    feat: 소셜 로그인 페이지 구현 및 API 연동

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import MovieDetail from "./pages/MovieDetail";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+
+/* 라우터 설정 추가 */
+const router = createBrowserRouter([
+  {
+    path: "/", // 기본 경로에서 Login 페이지로 이동
+    element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+  {
+    path: "/movie-detail",
+    element: <MovieDetail />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className='app-container'>
+      <RouterProvider router={router} />
+    </section>
   )
 }
 
