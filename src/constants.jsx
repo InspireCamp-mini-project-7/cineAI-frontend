d3ae9c347e334cfa99b659ae83f4ed65c1af61a6
zzzzzisu <kimjisu4939@gmail.com>
2025-02-22 11:25:58 +0900

    chore: 로고 이미지 src 통일

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,5 +1,5 @@
-// 공통 상수 관리리
+// 공통 상수 관리
 export const API_KEY = "5733CS321TJ154JO0103";
 export const BASE_URL = "/api/openapi-data2/wisenut/search_api/search_json2.jsp";
 export const PLACEHOLDER_IMAGE = "https://placehold.co/200x300?text=No+Image"; // 서비스 변경
-export const LOGO_IMAGE = "../Logo image.png";
+export const LOGO_IMAGE = "./src/assets/cineaiLogo.png";
