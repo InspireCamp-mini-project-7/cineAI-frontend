4b46f0d45c5fb45d54a16202796552cabbd41ee4
zzzzzisu <kimjisu4939@gmail.com>
2025-02-24 15:18:33 +0900

    feat: 관리자 페이지 구현 및 API 연동

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Admin.css'
+import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Admin = () => {
-  return (
-    <div>Admin</div>
-  )
+
+    // 최신 영화 목록 저장
+    const [newMovieList, setNewMovieList] = useState([]);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      getNewMovies();
+    }, [])
+
+    // 최신 영화 목록 불러오는 함수
+    const getNewMovies = async () => {
+      try {
+        const serverResponse =  await axios.get("http://localhost:8080/movies/new-movies?&size=30");
+        setNewMovieList(serverResponse.data.data.content);
+        console.log("new movie list : ", serverResponse.data.data.content);
+      }
+      catch (error) {
+        console.error("최신 영화 목록 가져오기 실패 : ", error);
+      }
+    } 
+
+    // 영화 추가 버튼 클릭 시 페이지 이동
+    const handleCreateButton = () => {
+      navigate('/admin/newMovie');
+    }
+
+    return (
+      <section className='admin-container'>
+          <header className='admin-header'>
+              <div className='admin-headerText'>관리자 페이지</div>
+          </header>
+          <div className='admin-title-container'>
+            <h2>최신 영화 정보 관리</h2>
+            <button className='admin-createButton' onClick={handleCreateButton}>영화 추가</button>
+          </div>
+
+          <div className='admin-movies-section'>
+            <div className="admin-movies-grid">
+              {newMovieList.map(movie => (
+                <div key={movie.movieId} className="admin-movie-card">
+                    {/* <Link to={`/movie/${movie.movieId}`}> */}
+                      <img
+                        src={movie.posterImageUrl}
+                        alt={movie.title}
+                        className="admin-movie-poster"
+                        onError={e => {
+                          e.target.src = '../src/assets/cineaiLogo.png'; }} />
+                      {/* </Link> */}
+                  <div className="movie-title">{movie.title || "제목 없음"}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+      </section>
+    )
 }
 
 export default Admin
---
+++ src/pages/NewMovie.jsx
@@ -0,0 +1,231 @@
+import React from 'react'
+import './NewMovie.css'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import Swal from 'sweetalert2'
+import axios from 'axios'
+
+const NewMovie = () => {
+    // 영화 정보를 요청 데이터 구조에 맞게 상태로 관리
+    const [movie, setMovie] = useState({
+        title: '',
+        posterImageUrl: '',
+        directorName: '',
+        castList: '',      
+        nation: '',
+        plot: '',
+        genreList: '',    
+        accAudiences: 0,  
+        releaseDate: ''
+    });
+
+    const navigate = useNavigate();
+
+    // 뒤로 가기 버튼 클릭 시, 관리자 최신 영화 목록 페이지로 이동
+    const handleBackButton = () => {
+        navigate('/admin');
+    }   
+
+    // 포스터 이미지 업로드
+    const handleUpload = () => {  
+
+        // API 완성 후 추가
+    }
+
+    // 초기화 버튼 클릭 시
+    const handleDeleteButton = async () => {
+        // 파일 삭제 추가 
+
+        const result = await Swal.fire({
+            title: "입력 내용을 삭제하시겠습니까?",
+            text: "삭제 후 복구가 불가능합니다.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "네, 삭제할래요",
+            cancelButtonText: "취소"
+        })
+
+        if(result.isConfirmed) {
+            setMovie({ 
+                title: '',
+                posterImageUrl: '',
+                directorName: '',
+                castList: '',      
+                nation: '',
+                plot: '',
+                genreList: '', 
+                accAudiences: 0,   
+                releaseDate: ''
+            })
+        }
+
+        Swal.fire({
+            icon: 'success',
+            title: '입력한 영화 정보 삭제 완료 !',
+            text: '입력한 영화 정보가 성공적으로 삭제되었습니다.' 
+        });
+    }
+
+    // 영화 추가 함수
+    const handleSaveButton = async () => {
+        const requestData = {
+            ...movie,
+            directorName: movie.directorName.split(',').map(item => item.trim()),
+            castList: movie.castList.split(',').map(item => item.trim()),
+            genreList: movie.genreList.split(',').map(item => item.trim())
+        };
+
+        console.log('요청 데이터 : ', requestData);
+
+        try {
+            await axios.post(
+                "http://localhost:8080/movies/create",
+                requestData,
+                {
+                    headers: {
+                      'Content-Type': 'application/json'
+                    }
+                });
+
+            Swal.fire({
+                icon: 'success',
+                title: '입력한 영화 정보 저장 완료 !',
+                text: '입력한 영화 정보가 성공적으로 저장되었습니다.' 
+            });
+
+            // 입력 필드 초기화
+            setMovie({ 
+                title: '',
+                posterImageUrl: '',
+                directorName: '',
+                castList: '',      
+                nation: '',
+                plot: '',
+                genreList: '', 
+                accAudiences: 0,   
+                releaseDate: ''
+            })
+        }
+        catch (error) {
+            console.error("영화 정보 저장 실패 : ", error);
+        }
+    }
+
+     return (
+        <section className='newmovie-container'>
+            <header className='newmovie-header'>
+                <img className='newmovie-backButton' src='../src/assets/backIcon.png' onClick={handleBackButton} />
+                <div className='newmovie-headerText'>관리자 페이지</div>
+            </header>
+            <h2>최신 영화 추가</h2>
+            <div className="newmovie-movie-info">
+                <div>
+                    <label>포스터 이미지 : </label>
+                    {/* <input
+                    type="text"
+                    placeholder="포스터 URL"
+                    value={movie.posterImageUrl}
+                    onChange={(e) =>
+                        setMovie({ ...movie, posterImageUrl: e.target.value })
+                    }
+                    /> */}
+                    <button className="newmovie-upload-btn" onClick={handleUpload}>
+                    📤
+                    </button>
+                </div>
+
+                <div>
+                    <label>제목 : </label>
+                    <input
+                    type="text"
+                    placeholder="영화 제목"
+                    value={movie.title}
+                    onChange={(e) =>
+                        setMovie({ ...movie, title: e.target.value })
+                    }
+                    />
+                </div>
+
+                <div>
+                    <label>감독 : </label>
+                    <input
+                    type="text"
+                    placeholder="감독 이름"
+                    value={movie.directorName}
+                    onChange={(e) =>
+                        setMovie({ ...movie, directorName: e.target.value })
+                    }
+                    />
+                </div>
+
+                <div>
+                    <label>출연 배우 (쉼표로 구분) : </label>
+                    <input
+                    type="text"
+                    placeholder="예: 레오나르도 디카프리오, 조셉 고든-레빗, 엘렌 페이지"
+                    value={movie.castList}
+                    onChange={(e) =>
+                        setMovie({ ...movie, castList: e.target.value })
+                    }
+                    />
+                </div>
+
+                <div>
+                    <label>국가 : </label>
+                    <input
+                    type="text"
+                    placeholder="제작 국가"
+                    value={movie.nation}
+                    onChange={(e) =>
+                        setMovie({ ...movie, nation: e.target.value })
+                    }
+                    />
+                </div>
+
+                <div>
+                    <label>장르 (쉼표로 구분) : </label>
+                    <input
+                    type="text"
+                    placeholder="예: 과학 영화, 액션, 모험"
+                    value={movie.genreList}
+                    onChange={(e) =>
+                        setMovie({ ...movie, genreList: e.target.value })
+                    }
+                    />
+                </div>
+
+                <div>
+                    <label>개봉년도 : </label>
+                    <input
+                    type="text"
+                    placeholder="예: 2010"
+                    value={movie.releaseDate}
+                    onChange={(e) =>
+                        setMovie({ ...movie, releaseDate: e.target.value })
+                    }
+                    />
+                </div>
+            </div>
+
+            <div className="newmovie-plot-section">
+                <h3>줄거리</h3>
+                <textarea
+                    placeholder="줄거리 입력"
+                    value={movie.plot}
+                    onChange={(e) =>
+                    setMovie({ ...movie, plot: e.target.value })
+                    }
+                ></textarea>
+            </div>
+
+            <div className='newmovie-button-container'>
+                <button className='newmovie-deleteButton' onClick={handleDeleteButton}>초기화</button>
+                <button className='newmovie-saveButton' onClick={handleSaveButton}>저장</button>
+            </div>
+        </section>
+    )
+}
+
+export default NewMovie
